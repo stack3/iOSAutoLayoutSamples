2016bec9af261dd7190f93725039002200ed1449
EIMEI <miyamoto@stack3.net>
2014-02-19 16:21:26 +0900

    Fixed BEGIN/END position.

--- iOS7AutoLayoutSamples/iOS7AutoLayoutSamples/STVerticalScrollViewController.m
+++ iOS7AutoLayoutSamples/iOS7AutoLayoutSamples/STVerticalScrollViewController.m
@@ -81,9 +81,11 @@
 #endif
     
     NSMutableString *string = [[NSMutableString alloc] initWithCapacity:1000];
+    [string appendString:@"--- BEGIN\n"];
     for (int i = 0; i < 10; i++) {
-        [string appendString:@"--- BEGIN\nLorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.\n--- END"];
+        [string appendString:@"Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."];
     }
+    [string appendString:@"\n--- END"];
     _contentView.textLabel.text = string;
 }
 
